d59cd010c15d89968eaff2b3a1351b37425d8f55
kaiST <phucnguyen17072005@gmail.com>
2025-03-25 00:45:40 +0700

    feat: Thêm quản lý sản phẩm nhưng chưa fix, chưa fix lỗi mũi tên sidebar

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -1,2 +1,66 @@
-<?php
-echo "đây là dashboard";
+<div class="row my-3">
+    <div class="col-3">
+        <div class="card m-3">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <span class="badge text-bg-primary">Tháng</span>
+                </div>
+                <h5 class="fs-5 text-center mt-2 small-header">ĐƠN HÀNG TRONG THÁNG</h5>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title fs-1 fw-light">19</h5>
+                <div class="card-text d-flex align-items-center justify-content-between">
+                    <span class="fs-6">Tăng trường so với tháng trước</span><span class="text-primary fs-5">58.3%</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card m-3">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <span class="badge text-bg-primary">Tổng số đơn hàng</span>
+                </div>
+                <h5 class="fs-5 text-center mt-2">ORDERS</h5>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title fs-1 fw-light">55</h5>
+                <div class="card-text d-flex align-items-center justify-content-between">
+                    <span class="fs-6">Số đơn hủy 2 chiếm</span><span class="text-primary fs-5">2.3%</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card m-3">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <span class="badge text-bg-primary">Total</span>
+                </div>
+                <h5 class="fs-5 text-center mt-2">TỔNG DOANH THU</h5>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title fs-1 fw-light">100.000.000đ</h5>
+                <div class="card-text d-flex align-items-center justify-content-between">
+                    <span class="fs-6">Tổng doanh thu</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card m-3">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <span class="badge text-bg-primary">Customer</span>
+                </div>
+                <h5 class="fs-5 text-center mt-2">TỔNG SỐ KHÁCH HÀNG</h5>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title fs-1 fw-light">19</h5>
+                <div class="card-text d-flex align-items-center justify-content-between">
+                    <span class="fs-6">Tổng số khách hàng</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
