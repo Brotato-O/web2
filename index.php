6df6f828318b57870f3f3a75ef79d79f617abd0d
HuyKhai <hyykhai2607@gmail.com>
2025-03-27 08:39:37 +0700

    sapxepfile

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./index/style.css">
     <link rel="stylesheet" href="./footer/footer.css">
+    <link rel="stylesheet" href="./header/header.css">
 </head>
 <body>
-    <?php include './index/header.php'; ?>
+    <?php include './header/header.php'; ?>
     <?php include './index/menu.php'; ?>
     <?php include './index/hero.php'?>
     <?php include './index/products.php'?>
--- login/login.php
+++ login/login.php
@@ -7,11 +7,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../index/style.css">
     <link rel="stylesheet" href="../footer/footer.css">
+    <link rel="stylesheet" href="../header/header.css">
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <?php
-        include '../index/header.php';
+        include '../header/header.php';
         include 'backtoHome.php';
         include 'loginUI.php';
         include '../footer/footer.php';
