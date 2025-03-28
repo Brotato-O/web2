979d055bb1b768adeddf46c0ca43a1430637fd75
kaiST <phucnguyen17072005@gmail.com>
2025-03-28 08:54:20 +0700

    fix: fix lỗi databse

---
+++ admin/pages/loginAdmin.php
@@ -0,0 +1,45 @@
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="row justify-content-center">
+        <div class="col-5 d-flex flex-column gap-3 me-5">
+            <div class="title-login fs-1 fw-semibold">Dashboard</div>
+            <div>Chào mừng bạn đến với Bảng điều khiển Quản trị!</div>
+            <div>
+                Vui lòng đăng nhập để truy cập và quản lý các chức năng cốt lõi của
+                trang web.
+            </div>
+            <div>
+                Tại đây, bạn có thể theo dõi hiệu suất, cập nhật nội dung, quản lý
+                người dùng và nhiều hơn nữa.
+            </div>
+            <div>
+                Hãy đảm bảo rằng thông tin đăng nhập của bạn được bảo mật và chỉ
+                những người có thẩm quyền mới được truy cập.
+            </div>
+            <div>Hãy bắt đầu và giữ cho hoạt động của bạn luôn suôn sẻ!</div>
+        </div>
+
+        <div class="col-5 pt-5 px-4 border rounded-3 bg-body-secondary">
+            <div>
+                <form class="needs-validation" novalidate>
+                    <div class="w-100 py-3">
+                        <input type="email" class="form-control fs-5" id="validationCustom01" placeholder="Email"
+                            required />
+                        <div class="invalid-feedback">* Email không được để trống!</div>
+                    </div>
+                    <div class="w-100 pb-4">
+                        <input type="password" class="form-control fs-5" id="validationCustom02" placeholder="Password"
+                            required />
+                        <div class="invalid-feedback">
+                            * Mật khẩu không được để trống!
+                        </div>
+                    </div>
+                    <button class="btn btn-primary w-100" type="submit">
+                        Đăng nhập
+                    </button>
+                </form>
+
+                <div class="py-3">Đăng nhập để vào trang quản trị</div>
+            </div>
+        </div>
+    </div>
+</div>
