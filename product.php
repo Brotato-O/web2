93847741d18a9186c39ce32ff1db4de0935610cf
tuantu004 <anhtuancogang123@gmail.com>
2025-04-03 09:05:25 +0700

    đưa header và footer vào file product.php

---
+++ product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản Lý Bán Hàng 1</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="./product/product.css">
+    <link rel="stylesheet" href="./footer/footer.css">
+    <link rel="stylesheet" href="./header/header.css">
+</head>
+<body>
+    <?php include './header/header.php'; ?>
+    <?php include './index/menu.php'; ?>
+    <?php include './index/hero.php'?>
+    <?php include './product/product.php'?>
+    <?php include './footer/footer.php'; ?>
+    <?php include 'backToTop.php'; ?>
+<script src="./index/script.js"></script>
+</body>
+</html>
